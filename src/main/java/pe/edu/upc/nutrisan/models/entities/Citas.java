5c42cae98dcbecce373c4c31892466876ee1c89a
carlos adrian alvarado jimenez <u201824455@upc.edu.pe>
2021-04-22 00:56:18 -0500

    version 1.0

---
+++ src/main/java/pe/edu/upc/nutrisan/models/entities/Citas.java
@@ -0,0 +1,129 @@
+package pe.edu.upc.nutrisan.models.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="Citas")
+@SequenceGenerator(name="getCitas",initialValue = 1,allocationSize = 1)
+public class Citas {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "getCitas")
+  @Column(name="id",columnDefinition = "NUMERIC(10)",nullable = false)
+ 
+  private Integer cita_id;
+
+ 
+
+  @ManyToOne(fetch=FetchType.LAZY)
+  @JoinColumn(name = "nutricionista_id", nullable = false)
+  private Nutricionista nutricionista_Id;
+
+
+
+  @ManyToOne(fetch=FetchType.LAZY)
+  @JoinColumn(name = "paciente_id", nullable = false)
+  private Paciente paciente_Id;
+
+  @Column(name="date",nullable = false)
+  private Date fecha;
+
+  @Column(name="trecomendacion",length=255,nullable = false)
+  private String recomendacion;
+
+  @ManyToOne(fetch=FetchType.LAZY)
+  @JoinColumn(name = "hora_inicio", nullable = false)
+
+  private Horario hora_Inicio;
+
+  @ManyToOne(fetch=FetchType.LAZY)
+  @JoinColumn(name="hora_fin",nullable = false)
+  private Horario hora_fin;
+  
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "paciente", nullable = false)
+  private Paciente paciente;
+
+public Integer getCita_id() {
+	return cita_id;
+}
+
+public void setCita_id(Integer cita_id) {
+	this.cita_id = cita_id;
+}
+
+public Nutricionista getNutricionista_Id() {
+	return nutricionista_Id;
+}
+
+public void setNutricionista_Id(Nutricionista nutricionista_Id) {
+	this.nutricionista_Id = nutricionista_Id;
+}
+
+public Paciente getPaciente_Id() {
+	return paciente_Id;
+}
+
+public void setPaciente_Id(Paciente paciente_Id) {
+	this.paciente_Id = paciente_Id;
+}
+
+public Date getFecha() {
+	return fecha;
+}
+
+public void setFecha(Date fecha) {
+	this.fecha = fecha;
+}
+
+public String getRecomendacion() {
+	return recomendacion;
+}
+
+public void setRecomendacion(String recomendacion) {
+	this.recomendacion = recomendacion;
+}
+
+public Horario getHora_Inicio() {
+	return hora_Inicio;
+}
+
+public void setHora_Inicio(Horario hora_Inicio) {
+	this.hora_Inicio = hora_Inicio;
+}
+
+public Horario getHora_fin() {
+	return hora_fin;
+}
+
+public void setHora_fin(Horario hora_fin) {
+	this.hora_fin = hora_fin;
+}
+
+public Paciente getPaciente() {
+	return paciente;
+}
+
+public void setPaciente(Paciente paciente) {
+	this.paciente = paciente;
+}
+
+ 
+
+
+
+  
+  
+}
---
+++ src/main/java/pe/edu/upc/nutrisan/models/entities/Paciente.java
@@ -0,0 +1,42 @@
+package pe.edu.upc.nutrisan.models.entities;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name="Pacientes",
+indexes = {@Index(columnList = "paciente_apellido,paciente_nombre",name = "paciente_index_apellido_nombre")},
+uniqueConstraints = {@UniqueConstraint(columnNames = {"paciente_email"})})
+public class Paciente {
+    @Id
+    @Column(name="paciente_dni",columnDefinition = "NUMERIC(9)", nullable=false)
+    private Integer dni;
+
+    @Column(name="paciente_password", length=60)
+    private String password;
+
+    @Column(name="paciente_nombre", length=30)
+    private String nombre;
+
+    @Column(name="paciente_apellido", length=30)
+    private String apellido;
+
+    @Column(name="paciente_email", length=60)
+    private String email;
+
+    @Column(name="paciente_registro")
+    @Temporal(TemporalType.DATE)
+    private Date registro;
+
+    @OneToMany(mappedBy = "paciente")
+    private List<Citas> cita_id;
+}
